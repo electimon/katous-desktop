02e813b301ad6efcf69c4a602fc95653d7a6a18a
Katou Megumi <electi@yzu.moe>
2026-03-10 00:37:11 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "GNOME2 revived";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    gnome2.url = "path:./gnome2-revived-nix";
+  };
+
+  outputs = { self, nixpkgs, gnome2, ... }:
+  {
+    nixosConfigurations.katous-desktop = gnome2.nixosConfigurations.gnomevm;
+  };
+}
